e79500a016b03877bc4462407e53d6aef526c59c
Rastilan <christoperbhall@gmail.com>
2022-05-22 23:45:23 -0400

    side bar collapse and expand working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './style.css';
 
-console.log("test");
+window.SideBarController = function() {
+    if(document.getElementsByClassName('left')[0].getAttribute('id') === 'expanded') {
+        document.getElementsByClassName('container')[0].style.gridTemplateColumns = '0vw auto';
+        document.getElementsByClassName('left')[0].setAttribute('id', 'collapsed');
+        document.getElementById('side-bar-controller').innerHTML = '>';
+    }   else {
+        document.getElementsByClassName('container')[0].style.gridTemplateColumns = '15vw auto';
+        document.getElementsByClassName('left')[0].setAttribute('id', 'expanded');
+        document.getElementById('side-bar-controller').innerHTML = '<';
+    }
+    
+    
+}
+
+window.ListConstructor = function() {
+    this.
+}
+
